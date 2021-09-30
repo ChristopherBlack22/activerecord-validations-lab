db6e7cef8e251d5e526e073848c8c83446b42ace
Christopher <christopher.black.2209@googlemail.com>
2021-09-30 01:29:39 +0100

    commit tree 50ba32421cb748893f7fcdb0f524d49bf794dce6

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+    validate :title_is_not_clickbait
+
+    def title_is_not_clickbait
+        clickbait = [/Won't Believe/, /Secret/, /Top \d/, /Guess/]
+        if clickbait.none? do |bait|
+            bait.match(title)
+            end 
+                errors.add(:title_is_not_clickbait, "Title isnt sufficently clickbait-y")
+        end
+    end 
 end
